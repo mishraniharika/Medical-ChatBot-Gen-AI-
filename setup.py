b63b7199f07e7bbc805f6500fde7bb627fab4529
mishraniharika <mishraniharika2327@gmail.com>
2025-04-26 01:18:16 +0530

    Folder Structure Added.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="AI Project",
+    version='0.0.0',
+    author='Niharika Mishra',
+    author_email='mishraniharika2327@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
